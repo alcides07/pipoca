81f62469d513176f36eafb565aa8aebc7a07e56b
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2023-11-30 22:07:23 -0300

    chore: utiliza retorno padronizado da API em classe de teste.

--- backend/routers/item.py
+++ backend/routers/item.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, Depends
-from schemas.item import Item_Read, Item_Create
+from schemas.item import Item_Create
 from schemas.common.pagination import pagination_schema
 from dependencies.router_parameters import pagination_router
 from dependencies.database import get_db
 from sqlalchemy.orm import Session
 from crud.item import create_item, read_items
+from schemas.common.return_body import return_schema
+from fastapi import status
+from fastapi.encoders import jsonable_encoder
+
 
 router = APIRouter(
     prefix="/items",
@@ -12,10 +16,16 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=list[Item_Read])
+@router.get("/", response_model=return_schema)
 def items(db: Session = Depends(get_db), common: pagination_schema = Depends(pagination_router)):
-    items = read_items(db, common)
-    return items
+    items = jsonable_encoder(read_items(db, common))
+
+    return {
+        "type": "success",
+        "message": "teste",
+        "status": status.HTTP_200_OK,
+        "data": items
+    }
 
 
 @router.post("/", response_model=Item_Create)
