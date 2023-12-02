2b932c93b8d86afc4a630c68364a4f82c64e8bb6
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2023-12-02 01:24:59 -0300

    removed: deleta classe de teste Item.

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from routers import item, user
+from routers import user
 from database import engine, Base
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-app.include_router(item.router)
 app.include_router(user.router)
