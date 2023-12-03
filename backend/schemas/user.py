a09e8de2e99edbff2c91153b35398e2dffdf484f
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2023-12-03 18:02:13 -0300

    docs: documenta schema de usuario. (#76)

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -2,13 +2,15 @@ from pydantic import BaseModel, Field, EmailStr
 
 
 class User_Base(BaseModel):
-    username: str = Field(max_length=32)
+    username: str = Field(max_length=32,
+                          description="Apelido do usuário")
     email: EmailStr()
 
 
 class User_Create(User_Base):
-    password: str = Field(max_length=32)
-    passwordConfirmation: str = Field(max_length=32)
+    password: str = Field(max_length=32, description="Senha do usuário")
+    passwordConfirmation: str = Field(
+        max_length=32, description="Confirmação da senha do usuário")
 
 
 class User_Read(User_Base):
