283f8c2c41b31303824b34fbd00e2f543dd4a748
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2023-12-03 18:00:58 -0300

    docs: documenta header UI do OpenAPI. (#76)

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
 from routers import user
 from database import engine, Base
+from fastapi.openapi.utils import get_openapi
 
 Base.metadata.create_all(bind=engine)
-app = FastAPI()
 
+app = FastAPI(swagger_ui_parameters={"syntaxHighlight.theme": "nord"})
 app.include_router(user.router)
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="API Juiz Online",
+        version="0.0.1",
+        summary="",
+        description="API em desenvolvimento",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
