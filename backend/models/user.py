b63edcb6f5826524afc60deca2f3570fde706a42
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2023-12-04 21:19:38 -0300

    lint: formata parametros do modelo user. (#85)

--- backend/models/user.py
+++ backend/models/user.py
@@ -5,7 +5,24 @@ from database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(length=32), index=True, unique=True)
-    email = Column(String(), index=True, unique=True)
-    password = Column(String(length=64), index=True)
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    username = Column(
+        String(length=32),
+        unique=True,
+        index=True
+    )
+
+    email = Column(
+        String(),
+        unique=True,
+        index=True
+    )
+
+    password = Column(
+        String(length=64)
+    )
