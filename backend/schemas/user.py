de11e7c614dc05d1d85dfa6d2ef1012031391fd4
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2023-12-04 21:19:54 -0300

    lint: formata parametros do schema user. (#85)

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -2,15 +2,24 @@ from pydantic import BaseModel, Field, EmailStr
 
 
 class User_Base(BaseModel):
-    username: str = Field(max_length=32,
-                          description="Apelido do usuário")
+    username: str = Field(
+        max_length=32,
+        description="Apelido do usuário"
+    )
+
     email: EmailStr()
 
 
 class User_Create(User_Base):
-    password: str = Field(max_length=32, description="Senha do usuário")
+    password: str = Field(
+        max_length=32,
+        description="Senha do usuário"
+    )
+
     passwordConfirmation: str = Field(
-        max_length=32, description="Confirmação da senha do usuário")
+        max_length=32,
+        description="Confirmação da senha do usuário"
+    )
 
 
 class User_Read(User_Base):
