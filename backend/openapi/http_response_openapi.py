dcbaeeb2ad43d7ee6d044ff0303e0967ce9eedad
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2023-12-09 16:56:43 -0300

    refactor: move metodo de http openapi para outro diretorio. (#89)

--- backend/openapi/http_response_openapi.py
+++ backend/openapi/http_response_openapi.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 
-def response_http_openapi(code: int | str, model: Any, description: str = ""):
+def http_response_openapi(code: int | str, model: Any, description: str = ""):
     return {
         code: {"model": model, "description": description}
     }
