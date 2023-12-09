6ee228b56a84e31e1f9275e916231f8f18651a33
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2023-12-09 16:59:25 -0300

    feat: cria classe de response com mensagem. (#89)

--- backend/schemas/common/response.py
+++ backend/schemas/common/response.py
@@ -1,9 +1,13 @@
 from pydantic import BaseModel
-from typing import TypeVar, List, Generic
+from typing import Optional, TypeVar, List, Generic
 
 T = TypeVar('T')
 
 
 class response_schema(BaseModel, Generic[T]):
-    message: str | None = None
     data: T | List[T] | None = None
+
+
+class response_schema_message(BaseModel, Generic[T]):
+    data: T | List[T] | None = None
+    message: Optional[str] = None
