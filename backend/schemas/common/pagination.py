c9ae76789a082c49cd80a956f7c01f486dff5009
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2023-12-11 21:42:08 -0300

    feat: implementa schemas de metadados de paginacao. (#91)

--- backend/schemas/common/pagination.py
+++ backend/schemas/common/pagination.py
@@ -1,4 +1,22 @@
 from fastapi import Query
+from enum import Enum
+from pydantic import BaseModel
+
+
+class Limit_Schema(int, Enum):
+    cinco = 5
+    dez = 10
+    quinze = 15
+    vinte = 20
+    vinte_cinco = 25
+    cinquenta = 50
+    cem = 100
+
+
+class Metadata_Schema(BaseModel):
+    count: int
+    limit: int
+    offset: int
 
 
 class Pagination_Schema:
@@ -6,12 +24,12 @@ class Pagination_Schema:
         self,
         q: str = Query(
             default=None, description="Palavras-chave para pesquisa"),
-        skip: int = Query(
-            default=None, description="Intervalo inicial da paginação"),
-        limit: int = Query(
-            default=None, description="Número de registros da paginação")
+        limit: Limit_Schema = Query(
+            default=10, description="Número de registros da paginação"),
+        offset: int = Query(
+            default=0, description="Intervalo inicial da paginação"),
 
     ):
         self.q = q
-        self.skip = skip
         self.limit = limit
+        self.offset = offset
