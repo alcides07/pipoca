a0a349ccf0cbf9b64c1683ada4e7ed5fb42541a8
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2023-12-11 21:44:32 -0300

    feat: implementa dados paginados em listagem de usuarios. (#91)

--- backend/orm/user.py
+++ backend/orm/user.py
@@ -1,12 +1,17 @@
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
-from schemas.common.pagination import Pagination_Schema
+from schemas.common.pagination import Metadata_Schema, Pagination_Schema
 from models.user import User
 from schemas.user import User_Create
 
 
 def read_users(db: Session, common: Pagination_Schema):
-    return db.query(User).offset(common.skip).limit(common.limit).all()
+    query = db.query(User).offset(common.offset).limit(common.limit).all()
+    count = db.query(User).count()
+    metadata = Metadata_Schema(
+        count=count, offset=common.offset, limit=common.limit)
+
+    return query, metadata
 
 
 def read_user_by_id(db: Session, id: int):
