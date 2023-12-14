37393a23bfea6f6e12f29b3a6a7131c06c4290f5
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2023-12-13 21:54:58 -0300

    feat: implementa rota de login com criacao de token. (#73)

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,17 +1,38 @@
-from fastapi import APIRouter, Depends, HTTPException, Path, status
-from schemas.auth import User_Login, User_Login_Out
-from schemas.common.exception import Exception_Schema
-from openapi.http_response_openapi import http_response_openapi
-from schemas.user import User_Create, User_Read
-from schemas.common.pagination import Pagination_Schema
+from fastapi import APIRouter, Depends, HTTPException, status
+from schemas.auth import User_Login_Out
 from dependencies.database import get_db
 from sqlalchemy.orm import Session
-from orm.user import create_user, read_user_by_id, read_user_by_key, read_users, user_by_key_exists, delete_user
-from schemas.common.response import Response_Schema_Pagination, Response_Schema_Unit
-from fastapi.encoders import jsonable_encoder
+from orm.user import read_user_by_key
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt
+from datetime import datetime, timedelta
+from decouple import config
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth")
+ALGORITHM = str(config("ALGORITHM"))
+TOKEN_EXPIRE_MINUTES = config("TOKEN_EXPIRE_MINUTES")
+
+
+def create_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    SECRET_KEY = str(config("SECRET_KEY"))
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def authenticate_user(db, credential: str, password: str):
+    def authenticate_with_key(key: str):
+        user_db = read_user_by_key(db, key, credential)
+        if user_db and verify_password(password, user_db.password):
+            return user_db
+        return False
+
+    return authenticate_with_key("username") or authenticate_with_key("email")
 
 
 def verify_password(password, hashed_password):
@@ -25,27 +46,24 @@ router = APIRouter(
 
 
 @router.post("/",
-             response_model=Response_Schema_Unit[User_Login_Out],
              summary="Autentica um usuário",
-             responses=http_response_openapi(
-                 status.HTTP_400_BAD_REQUEST,
-                 Exception_Schema,
-             ))
-def login(
-    user: User_Login,
+             response_model=User_Login_Out
+             )
+async def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
     db: Session = Depends(get_db),
 ):
-    user_db = read_user_by_key(db, "username", user.credential)
-    if (user_db != None):
-        if not verify_password(user.password, user_db.password):
-            print("senha invalida 1!")
-            return
-        print("tudo certo via username!")
-
-    else:
-        user_db = read_user_by_key(db, "email", user.credential)
-        if (user_db != None):
-            if not verify_password(user.password, user_db.password):
-                print("senha invalida 2!")
-                return
-            print("deu certo via email!")
+    user = authenticate_user(
+        db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Erro. Credenciais inválidas!",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=TOKEN_EXPIRE_MINUTES)
+    access_token = create_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    data = User_Login_Out(access_token=access_token, token_type="bearer")
+    return data
