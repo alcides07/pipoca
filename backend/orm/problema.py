e71e952dac49fafad0e0b1f41897ac68b12dbc35
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-01-04 21:17:48 -0300

    feat: readapta orm de problema para declaracao. (#108)

--- backend/orm/problema.py
+++ backend/orm/problema.py
@@ -1,12 +1,20 @@
 from sqlalchemy.orm import Session
+from models.declaracao import Declaracao
 from models.problema import Problema
 from models.tag import Tag
 from schemas.problema import ProblemaCreate
 
 
 def create_problema(db: Session, problema: ProblemaCreate):
-    db_problema = Problema(**problema.model_dump(exclude=set(["tags"])))
+    db_problema = Problema(
+        **problema.model_dump(exclude=set(["tags", "declaracoes"])))
     db.add(db_problema)
+
+    for declaracao in problema.declaracoes:
+        db_declaracao = Declaracao(
+            **declaracao.model_dump())
+        db.add(db_declaracao)
+        db_problema.declaracoes.append(db_declaracao)
 
     for tag in problema.tags:
         db_tag = db.query(Tag).filter(Tag.nome == tag).first()
