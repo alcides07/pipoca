aab2b5201bf2ea6cecb8d72487c33e6027f7a22b
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-01-04 21:16:14 -0300

    feat: implementa modelo de declaracao. (#108)

---
+++ backend/models/declaracao.py
@@ -0,0 +1,48 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from database import Base
+from sqlalchemy.orm import relationship
+
+
+class Declaracao(Base):
+    __tablename__ = "declaracoes"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    titulo = Column(
+        String(length=64),
+        index=True,
+        nullable=False,
+    )
+
+    contextualizacao = Column(
+        String(length=10240),
+        nullable=False,
+    )
+
+    formatacao_entrada = Column(
+        String(length=10240),
+        nullable=False,
+    )
+
+    formatacao_saida = Column(
+        String(length=10240),
+        nullable=False,
+    )
+
+    observacao = Column(
+        String(length=10240),
+    )
+
+    tutorial = Column(
+        String(length=80240),
+    )
+
+    problema_id = Column(Integer, ForeignKey('problemas.id'))
+    problema = relationship(
+        "Problema",
+        back_populates="declaracoes",
+    )
