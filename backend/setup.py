78b5eaed8ef2b1152365cb6f62ac7be2bcd71135
Alcides Dantas <alcidesdantasdj@gmail.com>
2024-01-07 02:10:26 -0300

    Update setup.py

--- backend/setup.py
+++ backend/setup.py
@@ -1,2 +1,4 @@
-from setuptools import setup, find_packages
-setup(name="backend", packages=find_packages())
+import setuptools
+
+setuptools.setup(name='backend',
+                 packages=setuptools.find_packages())
