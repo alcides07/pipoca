5d8808f9f99b1ebc3904e4e8edf882e1ed9d0e77
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-01-11 19:42:54 -0300

    test: incrementa teste de problema com verificador. (#140)

--- backend/tests/integration/test_problema.py
+++ backend/tests/integration/test_problema.py
@@ -3,24 +3,18 @@ from fastapi.testclient import TestClient
 from tests.config_test import remove_dependencies, resume_dependencies
 
 client = TestClient(app)
+
 URL_PROBLEMA = "/problemas"
-
-
-def test_read_problema_unit():
-    remove_dependencies()
-
-    response = client.post(
-        URL_PROBLEMA,
-        json={
-            "nome": "string",
-            "nome_arquivo_entrada": "string",
+JSON_PROBLEMA = {
+    "nome": "string",
+    "nome_arquivo_entrada": "string",
             "nome_arquivo_saida": "string",
             "tempo_limite": 250,
             "memoria_limite": 600,
             "tags": [
                 "string"
             ],
-            "declaracoes": [
+    "declaracoes": [
                 {
                     "titulo": "string",
                     "contextualizacao": "string",
@@ -31,14 +25,27 @@ def test_read_problema_unit():
                     "idioma": "Africâner"
                 }
             ],
-            "arquivos": [
+    "arquivos": [
                 {
                     "nome": "string",
                     "corpo": "string",
                     "secao": "recursos"
                 }
-            ]
-        }
+            ],
+    "verificador": {
+                "nome": "string",
+                "linguagem": "string",
+                "corpo": "string"
+            }
+}
+
+
+def test_read_problema_unit():
+    remove_dependencies()
+
+    response = client.post(
+        URL_PROBLEMA,
+        json=JSON_PROBLEMA
     )
 
     problema_id = response.json().get("data", {}).get("id", None)
@@ -64,34 +71,7 @@ def test_create_problema():
 
     response = client.post(
         URL_PROBLEMA,
-        json={
-            "nome": "string",
-            "nome_arquivo_entrada": "string",
-            "nome_arquivo_saida": "string",
-            "tempo_limite": 250,
-            "memoria_limite": 600,
-            "tags": [
-                "string"
-            ],
-            "declaracoes": [
-                {
-                    "titulo": "string",
-                    "contextualizacao": "string",
-                    "formatacao_entrada": "string",
-                    "formatacao_saida": "string",
-                    "observacao": "string",
-                    "tutorial": "string",
-                    "idioma": "Africâner"
-                }
-            ],
-            "arquivos": [
-                {
-                    "nome": "string",
-                    "corpo": "string",
-                    "secao": "recursos"
-                }
-            ]
-        }
+        json=JSON_PROBLEMA
     )
     assert response.status_code == 201
 
