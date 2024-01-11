b6ac1fe714336f7fac9a9b30be4cea8ea6b397bb
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-01-10 21:44:22 -0300

    test: implementa teste de integracao de leitura de problemas. (#138)

--- backend/tests/integration/test_problema.py
+++ backend/tests/integration/test_problema.py
@@ -5,6 +5,59 @@ from tests.config_test import remove_dependencies, resume_dependencies
 client = TestClient(app)
 
 
+def test_read_problema_unit():
+    remove_dependencies()
+
+    response = client.post(
+        "/problemas",
+        json={
+            "nome": "string",
+            "nome_arquivo_entrada": "string",
+            "nome_arquivo_saida": "string",
+            "tempo_limite": 250,
+            "memoria_limite": 600,
+            "tags": [
+                "string"
+            ],
+            "declaracoes": [
+                {
+                    "titulo": "string",
+                    "contextualizacao": "string",
+                    "formatacao_entrada": "string",
+                    "formatacao_saida": "string",
+                    "observacao": "string",
+                    "tutorial": "string",
+                    "idioma": "Africâner"
+                }
+            ],
+            "arquivos": [
+                {
+                    "nome": "string",
+                    "corpo": "string",
+                    "secao": "recursos"
+                }
+            ]
+        }
+    )
+
+    problema_id = response.json().get("data", {}).get("id", None)
+    response = client.get(f"/problemas/{problema_id}/")
+    assert response.status_code == 200
+
+    resume_dependencies()
+
+
+def test_read_problemas():
+    remove_dependencies()
+
+    response = client.get(
+        "/problemas",
+    )
+    assert response.status_code == 200
+
+    resume_dependencies()
+
+
 def test_create_problema():
     remove_dependencies()
 
