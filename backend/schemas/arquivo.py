9bbcf11ab101c4c2b8d03b6cfa5ee317f969004e
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-01-10 21:04:49 -0300

    feat: implementa importacao de arquivos de solucao com status. (#136)

--- backend/schemas/arquivo.py
+++ backend/schemas/arquivo.py
@@ -7,8 +7,9 @@ PROBLEMA_ID_DESCRIPTION = "Identificador do problema associado ao arquivo"
 
 class SecaoSchema(Enum):
     RECURSO = "recursos"
-    FONTE = "arquivos fonte"
+    FONTE = "arquivos_fonte"
     ANEXO = "anexo"
+    SOLUCAO = "solucao"
 
 
 class ArquivoBase(BaseModel):
@@ -18,36 +19,38 @@ class ArquivoBase(BaseModel):
         description="Nome do arquivo do problema"
     )
 
+    secao: SecaoSchema = Field(
+        description="Grupo o qual o arquivo faz parte"
+    )
+
+    status: str | None = Field(default=None,
+                               description="Tipos de status de veredíto para arquivos de solução"
+                               )
+
+
+class ArquivoWithBody(ArquivoBase):
     corpo: str = Field(
         max_length=250000,
         description="Conteúdo do arquivo"
     )
 
-    secao: SecaoSchema = Field(
-        description="Grupo o qual o arquivo faz parte"
-    )
 
-
-class ArquivoRead(ArquivoBase):
+class ArquivoReadFull(ArquivoWithBody):
     id: int = Field(description=ARQUIVO_ID_DESCRIPTION)
     problema_id: int = Field(
         description=PROBLEMA_ID_DESCRIPTION)
 
 
-class ArquivoReadSimple(BaseModel):
-    nome: str = Field(
-        max_length=64,
-        description="Nome do arquivo do problema"
-    )
+class ArquivoReadSimple(ArquivoBase):
     id: int = Field(description=ARQUIVO_ID_DESCRIPTION)
     problema_id: int = Field(
         description=PROBLEMA_ID_DESCRIPTION)
 
 
-class ArquivoCreate(ArquivoBase):
+class ArquivoCreate(ArquivoWithBody):
     pass
 
 
-class ArquivoCreateSingle(ArquivoBase):
+class ArquivoCreateSingle(ArquivoWithBody):
     problema_id: int = Field(
         description=PROBLEMA_ID_DESCRIPTION)
