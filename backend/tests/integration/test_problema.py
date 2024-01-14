e94c7697711939fa289375786e8af817ff0f5251
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-01-13 21:22:19 -0300

    test: implementa testes de integracao de PUT e PATCH de problema. (#130)

--- backend/tests/integration/test_problema.py
+++ backend/tests/integration/test_problema.py
@@ -41,19 +41,21 @@ JSON_PROBLEMA = {
                 "nome": "string",
                 "linguagem": "string",
                 "corpo": "string"
-            }
+            },
+    "tags": ["1", "2"]
 }
 
 
 def test_read_problema_unit():
     remove_dependencies()
+    json = JSON_PROBLEMA.copy()
 
     response = client.post(
         URL_PROBLEMA,
-        json=JSON_PROBLEMA
+        json=json
     )
 
-    problema_id = response.json().get("data", {}).get("id", None)
+    problema_id = response.json().get("data").get("id")
     response = client.get(f"{URL_PROBLEMA}/{problema_id}/")
     assert response.status_code == 200
 
@@ -73,11 +75,114 @@ def test_read_problemas():
 
 def test_create_problema():
     remove_dependencies()
+    json = JSON_PROBLEMA.copy()
 
     response = client.post(
         URL_PROBLEMA,
-        json=JSON_PROBLEMA
+        json=json
     )
     assert response.status_code == 201
 
     resume_dependencies()
+
+
+def test_update_partial_problema():
+    remove_dependencies()
+    json = JSON_PROBLEMA.copy()
+
+    response_post = client.post(
+        URL_PROBLEMA,
+        json=json
+    ).json().get("data")
+
+    problema_id = response_post.get("id")
+
+    json_partial = {
+        "nome": "NovoNome",
+        "tags": ["Tag1, Tag2"]
+    }
+
+    response_patch = client.patch(
+        f"{URL_PROBLEMA}/{problema_id}/",
+        json=json_partial
+    )
+
+    assert response_patch.status_code == 200
+
+    response_patch_json = response_patch.json().get("data")
+
+    assert response_patch_json.get(
+        "nome") != response_post.get("nome")
+
+    assert response_patch_json.get(
+        "tags") != response_post.get("tags")
+
+    assert response_patch_json.get(
+        "tempo_limite") == response_post.get("tempo_limite")
+
+    resume_dependencies()
+
+
+def test_update_full_problema_success():
+    remove_dependencies()
+    json_full = JSON_PROBLEMA.copy()
+
+    response_post = client.post(
+        URL_PROBLEMA,
+        json=json_full
+    ).json().get("data")
+
+    problema_id = response_post.get("id")
+
+    json_full["nome"] = "PUT"
+    json_full["nome_arquivo_entrada"] = "PUT"
+    json_full["nome_arquivo_saida"] = "PUT"
+    json_full.pop("tags")
+
+    response_put = client.put(
+        f"{URL_PROBLEMA}/{problema_id}/",
+        json=json_full
+    )
+
+    assert response_put.status_code == 200
+
+    response_put_json = response_put.json().get("data")
+
+    assert response_put_json.get("nome") != response_post.get("nome")
+
+    assert response_put_json.get(
+        "nome_arquivo_entrada") != response_post.get("nome_arquivo_entrada")
+
+    assert response_put_json.get(
+        "nome_arquivo_saida") != response_post.get("nome_arquivo_saida")
+
+    assert response_put_json.get(
+        "tempo_limite") == response_post.get("tempo_limite")
+
+    assert response_put_json.get(
+        "memoria_limite") == response_post.get("memoria_limite")
+
+    resume_dependencies()
+
+
+def test_update_full_problema_fail():
+    remove_dependencies()
+    json = JSON_PROBLEMA.copy()
+
+    response_post = client.post(
+        URL_PROBLEMA,
+        json=json
+    ).json().get("data")
+
+    problema_id = response_post.get("id")
+
+    json_full_fake = {
+        "nome": "nome",
+    }
+
+    response_put = client.put(
+        f"{URL_PROBLEMA}/{problema_id}/",
+        json=json_full_fake
+    )
+
+    assert response_put.status_code == 422
