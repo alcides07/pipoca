24a480f4499e3498e6e685b02628c33de08f0737
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-01-23 21:30:53 -0300

    refactor: reutiliza utilitarios de problema em testes. (#113)

--- backend/tests/integration/test_problema.py
+++ backend/tests/integration/test_problema.py
@@ -1,80 +1,22 @@
+from passlib.context import CryptContext
+from tests.helpers.problema import URL_PROBLEMA, create_problema_helper
+from tests.helpers.user import create_user_helper
 from backend.main import app
 from fastapi.testclient import TestClient
 from tests.config_test import remove_dependencies, resume_dependencies
 
 client = TestClient(app)
-
-URL_PROBLEMA = "/problemas"
-JSON_PROBLEMA = {
-    "nome": "string",
-    "privado": True,
-    "nome_arquivo_entrada": "string",
-    "nome_arquivo_saida": "string",
-    "tempo_limite": 250,
-    "memoria_limite": 600,
-    "declaracoes": [
-                {
-                    "titulo": "string",
-                    "contextualizacao": "string",
-                    "formatacao_entrada": "string",
-                    "formatacao_saida": "string",
-                    "observacao": "string",
-                    "tutorial": "string",
-                    "idioma": "Africâner"
-                }
-    ],
-    "arquivos": [
-        {
-            "nome": "string",
-                    "corpo": "string",
-                    "secao": "recursos"
-        }
-    ],
-    "verificador": {
-        "nome": "string",
-                "linguagem": "string",
-                "corpo": "string",
-        "testes": [
-            {
-                "numero": "string",
-                "entrada": "string",
-                "veredito": "ok"
-            }
-        ]
-    },
-    "validador": {
-        "nome": "string",
-                "linguagem": "string",
-                "corpo": "string",
-        "testes": [
-            {
-                "numero": "string",
-                "entrada": "string",
-                "veredito": "valid"
-
-            },
-            {
-                "numero": "string",
-                "entrada": "string",
-                "veredito": "invalid"
-            },
-        ]
-    },
-    "tags": ["1", "2"]
-}
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def test_read_problema_unit():
     remove_dependencies()
-    json = JSON_PROBLEMA.copy()
 
-    response = client.post(
-        URL_PROBLEMA,
-        json=json
-    )
+    problema, _ = create_problema_helper()
 
-    problema_id = response.json().get("data").get("id")
+    problema_id = problema.json().get("data").get("id")
     response = client.get(f"{URL_PROBLEMA}/{problema_id}/")
+
     assert response.status_code == 200
 
     resume_dependencies()
@@ -86,6 +28,7 @@ def test_read_problemas():
     response = client.get(
         URL_PROBLEMA,
     )
+
     assert response.status_code == 200
 
     resume_dependencies()
@@ -93,106 +36,99 @@ def test_read_problemas():
 
 def test_create_problema():
     remove_dependencies()
-    json = JSON_PROBLEMA.copy()
 
-    response = client.post(
-        URL_PROBLEMA,
-        json=json
-    )
-    assert response.status_code == 201
+    problema, _ = create_problema_helper()
+
+    assert problema.status_code == 201
 
     resume_dependencies()
 
 
 def test_update_partial_problema():
     remove_dependencies()
-    json = JSON_PROBLEMA.copy()
 
-    response_post = client.post(
-        URL_PROBLEMA,
-        json=json
-    ).json().get("data")
-
-    problema_id = response_post.get("id")
+    problema, token = create_problema_helper()
+    problema = problema.json().get("data")
+    problema_id = problema.get("id")
 
     json_partial = {
         "nome": "NovoNome",
         "tags": ["Tag1, Tag2"]
     }
 
-    response_patch = client.patch(
+    response = client.patch(
         f"{URL_PROBLEMA}/{problema_id}/",
-        json=json_partial
+        json=json_partial,
+        headers={
+            "Authorization": f"Bearer {token}",
+        },
     )
 
-    assert response_patch.status_code == 200
+    assert response.status_code == 200
 
-    response_patch_json = response_patch.json().get("data")
+    response_json = response.json().get("data")
 
-    assert response_patch_json.get(
-        "nome") != response_post.get("nome")
+    assert response_json.get(
+        "nome") != problema.get("nome")
 
-    assert response_patch_json.get(
-        "tags") != response_post.get("tags")
+    assert response_json.get(
+        "tags") != problema.get("tags")
 
-    assert response_patch_json.get(
-        "tempo_limite") == response_post.get("tempo_limite")
+    assert response_json.get(
+        "tempo_limite") == problema.get("tempo_limite")
 
     resume_dependencies()
 
 
 def test_update_full_problema_success():
     remove_dependencies()
-    json_full = JSON_PROBLEMA.copy()
 
-    response_post = client.post(
-        URL_PROBLEMA,
-        json=json_full
-    ).json().get("data")
+    problema, token = create_problema_helper()
+    problema = problema.json().get("data")
+    problema_id = problema.get("id")
 
-    problema_id = response_post.get("id")
+    problema_copy = problema.copy()
 
-    json_full["nome"] = "PUT"
-    json_full["nome_arquivo_entrada"] = "PUT"
-    json_full["nome_arquivo_saida"] = "PUT"
-    json_full.pop("tags")
+    problema["nome"] = "PUT"
+    problema["nome_arquivo_entrada"] = "PUT"
+    problema["nome_arquivo_saida"] = "PUT"
+    problema.pop("tags")
 
-    response_put = client.put(
+    response = client.put(
         f"{URL_PROBLEMA}/{problema_id}/",
-        json=json_full
+        headers={
+            "Authorization": f"Bearer {token}",
+        },
+        json=problema
     )
 
-    assert response_put.status_code == 200
+    assert response.status_code == 200
 
-    response_put_json = response_put.json().get("data")
+    response_json = response.json().get("data")
 
-    assert response_put_json.get("nome") != response_post.get("nome")
+    assert response_json.get("nome") != problema_copy.get("nome")
 
-    assert response_put_json.get(
-        "nome_arquivo_entrada") != response_post.get("nome_arquivo_entrada")
+    assert response_json.get(
+        "nome_arquivo_entrada") != problema_copy.get("nome_arquivo_entrada")
 
-    assert response_put_json.get(
-        "nome_arquivo_saida") != response_post.get("nome_arquivo_saida")
+    assert response_json.get(
+        "nome_arquivo_saida") != problema_copy.get("nome_arquivo_saida")
 
-    assert response_put_json.get(
-        "tempo_limite") == response_post.get("tempo_limite")
+    assert response_json.get(
+        "tempo_limite") == problema_copy.get("tempo_limite")
 
-    assert response_put_json.get(
-        "memoria_limite") == response_post.get("memoria_limite")
+    assert response_json.get(
+        "memoria_limite") == problema_copy.get("memoria_limite")
 
     resume_dependencies()
 
 
 def test_update_full_problema_fail():
     remove_dependencies()
-    json = JSON_PROBLEMA.copy()
 
-    response_post = client.post(
-        URL_PROBLEMA,
-        json=json
-    ).json().get("data")
+    problema, token = create_problema_helper()
 
-    problema_id = response_post.get("id")
+    problema_id = problema.json().get("data").get("id")
 
     json_full_fake = {
         "nome": "nome",
@@ -200,6 +136,9 @@ def test_update_full_problema_fail():
 
     response_put = client.put(
         f"{URL_PROBLEMA}/{problema_id}/",
+        headers={
+            "Authorization": f"Bearer {token}",
+        },
         json=json_full_fake
     )
 
@@ -209,11 +148,16 @@ def test_update_full_problema_fail():
 def test_upload_problema():
     remove_dependencies()
 
+    _, token = create_user_helper()
+
     with open("./tests/integration/upload_problem.zip", 'rb') as file:
         response = client.post(
             f"{URL_PROBLEMA}/upload/",
             files={"pacote": file},
             data={"privado": "true"},
+            headers={
+                "Authorization": f"Bearer {token}",
+            },
         )
 
     assert response.status_code == 201
