8e38a7b504485e52304e33f5280c56ced432b023
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-01-23 21:30:17 -0300

    refactor: reutiliza utilitarios de user em testes. (#113)

--- backend/tests/integration/test_user.py
+++ backend/tests/integration/test_user.py
@@ -1,35 +1,22 @@
+from tests.helpers.user import URL_USER, create_user_helper
 from backend.main import app
 from fastapi.testclient import TestClient
 from tests.config_test import remove_dependencies, resume_dependencies
 from passlib.context import CryptContext
-import os
-import base64
 
 client = TestClient(app)
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-URL_USER = "/users"
 
 
 def test_read_user_unit():
     remove_dependencies()
 
-    key = os.urandom(16)
-    value = base64.b64encode(key).decode()
-    password = pwd_context.hash(value)
+    user, _ = create_user_helper()
+    user = user.json().get("data")
 
-    response = client.post(
-        URL_USER,
-        json={
-            "username": value,
-            "email": f"{value}@email.com",
-            "password": password,
-            "passwordConfirmation": password
-
-        }
-    )
-
-    user_id = response.json().get("data", {}).get("id", None)
+    user_id = user.get("id")
     response = client.get(f"{URL_USER}/{user_id}/")
+
     assert response.status_code == 200
 
     resume_dependencies()
@@ -41,24 +28,13 @@ def test_read_users():
     response = client.get(
         URL_USER,
     )
+
     assert response.status_code == 200
 
     resume_dependencies()
 
 
 def test_create_user():
-    key = os.urandom(16)
-    value = base64.b64encode(key).decode()
-    password = pwd_context.hash(value)
+    user, _ = create_user_helper()
 
-    response = client.post(
-        URL_USER,
-        json={
-            "username": value,
-            "email": f"{value}@email.com",
-            "password": password,
-            "passwordConfirmation": password
-
-        }
-    )
-    assert response.status_code == 201
+    assert user.status_code == 201
