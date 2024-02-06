af3d89cb2424a2386b8dfe808f5f3badcd3812f1
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-02-05 22:35:44 -0300

    tests: implementa testes criacao de user com email e username existentes. (#113)

--- backend/tests/integration/test_user.py
+++ backend/tests/integration/test_user.py
@@ -1,3 +1,5 @@
+import os
+import base64
 from tests.helpers.user import URL_USER, create_user_helper
 from backend.main import app
 from fastapi.testclient import TestClient
@@ -43,3 +45,59 @@ def test_create_user():
     user, _ = create_user_helper()
 
     assert user.status_code == 201
+
+
+def test_create_user_email_exists():
+    key = os.urandom(16)
+    value = base64.b64encode(key).decode()
+    password = pwd_context.hash(value)
+
+    JSON_USER = {
+        "username": value,
+        "email": f"{value}@email.com",
+        "password": password,
+        "passwordConfirmation": password
+    }
+
+    client.post(
+        URL_USER,
+        json=JSON_USER
+    )
+
+    user_repeat = JSON_USER
+    user_repeat["username"] = "unique_username"
+
+    response = client.post(
+        URL_USER,
+        json=user_repeat
+    )
+
+    assert response.status_code == 400
+
+
+def test_create_user_username_exists():
+    key = os.urandom(16)
+    value = base64.b64encode(key).decode()
+    password = pwd_context.hash(value)
+
+    JSON_USER = {
+        "username": value,
+        "email": f"{value}@email.com",
+        "password": password,
+        "passwordConfirmation": password
+    }
+
+    client.post(
+        URL_USER,
+        json=JSON_USER
+    )
+
+    user_repeat = JSON_USER
+    user_repeat["email"] = "unique_email@email.com"
+
+    response = client.post(
+        URL_USER,
+        json=user_repeat
+    )
+
+    assert response.status_code == 400
