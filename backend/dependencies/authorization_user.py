1c65dcbedc2a62df9117126b731404384162ebdb
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-02-05 22:44:04 -0300

    refactor: unifica duas condicoes em uma. (#113)

--- backend/dependencies/authorization_user.py
+++ backend/dependencies/authorization_user.py
@@ -24,10 +24,11 @@ async def has_authorization_user(
 
     user = await user_autenthicated(token, db)
 
-    if (isinstance(obj_model_has_user_key, User) and user.id == obj_model_has_user_key.id):
-        return True
-
-    elif (hasattr(obj_model_has_user_key, "usuario_id") and obj_model_has_user_key.usuario_id == user.id):
+    if (
+        isinstance(obj_model_has_user_key,
+                   User) and user.id == obj_model_has_user_key.id
+        or
+            hasattr(obj_model_has_user_key, "usuario_id") and obj_model_has_user_key.usuario_id == user.id):
         return True
 
     return False
