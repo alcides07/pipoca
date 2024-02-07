b3abcc92426a99e998441754047237b3fadf8bbc
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-02-06 22:18:17 -0300

    tests: implementa testes de admin em arquivo. (#184)

--- backend/tests/integration/test_arquivo.py
+++ backend/tests/integration/test_arquivo.py
@@ -1,3 +1,6 @@
+from tests.database import get_db_test
+from tests.helpers.administrador import create_administrador_helper
+from tests.helpers.user import create_user_helper
 from backend.main import app
 from fastapi.testclient import TestClient
 from tests.config_test import remove_dependencies, resume_dependencies
@@ -5,12 +8,34 @@ from tests.config_test import remove_dependencies, resume_dependencies
 client = TestClient(app)
 
 
-def test_read_arquivos():
+def test_read_arquivos_user():
     remove_dependencies()
 
-    response = client.get(
+    _, token_user, _ = create_user_helper()
+
+    response_user = client.get(
         "/arquivos",
+        headers={
+            "Authorization": f"Bearer {token_user}",
+        },
     )
-    assert response.status_code == 200
+    assert response_user.status_code == 401
+
+    resume_dependencies()
+
+
+def test_read_arquivos_admin():
+    remove_dependencies()
+
+    database = next(get_db_test())
+    token_admin = create_administrador_helper(database)
+
+    response_admin = client.get(
+        "/arquivos",
+        headers={
+            "Authorization": f"Bearer {token_admin}",
+        },
+    )
+    assert response_admin.status_code == 200
 
     resume_dependencies()
