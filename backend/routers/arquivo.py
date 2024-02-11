421c7332c0ac0d6ece92f457120988f3f915f71a
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-02-10 21:58:35 -0300

    feat: implementa rota de excluir arquivo. (#204)

--- backend/routers/arquivo.py
+++ backend/routers/arquivo.py
@@ -2,9 +2,9 @@ from models.problema import Problema
 from routers.auth import oauth2_scheme
 from fastapi import APIRouter, Depends, Path
 from models.arquivo import Arquivo
-from schemas.arquivo import ArquivoReadFull, ArquivoReadSimple
+from schemas.arquivo import ARQUIVO_ID_DESCRIPTION, ArquivoReadFull, ArquivoReadSimple
 from utils.errors import errors
-from orm.common.index import get_by_id, get_all
+from orm.common.index import delete_object, get_by_id, get_all
 from dependencies.authenticated_user import get_authenticated_user
 from schemas.common.pagination import PaginationSchema
 from dependencies.database import get_db
@@ -49,7 +49,7 @@ async def read(
             }
             )
 async def read_id(
-        id: int = Path(description="Identificador do arquivo"),
+        id: int = Path(description=ARQUIVO_ID_DESCRIPTION),
         db: Session = Depends(get_db),
     token: str = Depends(oauth2_scheme)
 ):
@@ -61,6 +61,32 @@ async def read_id(
         model_has_user_key=Problema
     )
 
+    return ResponseUnitSchema(
+        data=arquivo
+    )
+
+
+@router.delete("/{id}/",
+               response_model=ResponseUnitSchema[ArquivoReadFull],
+               summary="Deleta um arquivo",
+               responses={
+                   404: errors[404]
+               },
+               dependencies=[Depends(get_authenticated_user)],
+               )
+async def delete(
+        id: int = Path(description=ARQUIVO_ID_DESCRIPTION),
+        db: Session = Depends(get_db),
+        token: str = Depends(oauth2_scheme)
+):
+
+    arquivo = await delete_object(
+        db=db,
+        model=Arquivo,
+        id=id,
+        token=token,
+        model_has_user_key=Problema
+    )
     return ResponseUnitSchema(
         data=arquivo
     )
