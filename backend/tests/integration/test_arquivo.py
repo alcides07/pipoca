12098147745ce5bd8633767b055e24924db3799c
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-02-10 23:12:26 -0300

    tests: implementa testes de leitura, criacao e exclusao de arquivo. (#204)

--- backend/tests/integration/test_arquivo.py
+++ backend/tests/integration/test_arquivo.py
@@ -1,11 +1,48 @@
 from tests.database import get_db_test
 from tests.helpers.administrador import create_administrador_helper
+from tests.helpers.arquivo import URL_ARQUIVO, create_arquivo_admin_helper, create_arquivo_user_helper
 from tests.helpers.user import create_user_helper
 from backend.main import app
 from fastapi.testclient import TestClient
 from tests.config_test import remove_dependencies, resume_dependencies
 
 client = TestClient(app)
+
+
+def test_read_arquivo_unit_user():
+    remove_dependencies()
+
+    response_arquivo_user, token = create_arquivo_user_helper()
+    arquivo_id = response_arquivo_user.json().get("data").get("id")
+
+    response = client.get(
+        f"{URL_ARQUIVO}/{arquivo_id}/",
+        headers={
+            "Authorization": f"Bearer {token}",
+        },
+    )
+
+    assert response.status_code == 200
+
+    resume_dependencies()
+
+
+def test_read_arquivo_unit_admin():
+    remove_dependencies()
+
+    response_arquivo_admin, token = create_arquivo_admin_helper()
+    arquivo_id = response_arquivo_admin.json().get("data").get("id")
+
+    response = client.get(
+        f"{URL_ARQUIVO}/{arquivo_id}/",
+        headers={
+            "Authorization": f"Bearer {token}",
+        },
+    )
+
+    assert response.status_code == 200
+
+    resume_dependencies()
 
 
 def test_read_arquivos_user():
@@ -37,5 +74,61 @@ def test_read_arquivos_admin():
         },
     )
     assert response_admin.status_code == 200
+
+    resume_dependencies()
+
+
+def test_create_arquivo_user():
+    remove_dependencies()
+
+    response, _ = create_arquivo_user_helper()
+
+    assert response.status_code == 201
+
+    resume_dependencies()
+
+
+def test_create_arquivo_admin():
+    remove_dependencies()
+
+    response, _ = create_arquivo_admin_helper()
+
+    assert response.status_code == 201
+
+    resume_dependencies()
+
+
+def test_delete_arquivo_user():
+    remove_dependencies()
+
+    response_arquivo_user, token = create_arquivo_user_helper()
+    arquivo_id = response_arquivo_user.json().get("data").get("id")
+
+    response = client.delete(
+        f"{URL_ARQUIVO}/{arquivo_id}/",
+        headers={
+            "Authorization": f"Bearer {token}",
+        },
+    )
+
+    assert response.status_code == 200
+
+    resume_dependencies()
+
+
+def test_delete_arquivo_admin():
+    remove_dependencies()
+
+    response_arquivo_admin, token = create_arquivo_admin_helper()
+    arquivo_id = response_arquivo_admin.json().get("data").get("id")
+
+    response = client.delete(
+        f"{URL_ARQUIVO}/{arquivo_id}/",
+        headers={
+            "Authorization": f"Bearer {token}",
+        },
+    )
+
+    assert response.status_code == 200
 
     resume_dependencies()
