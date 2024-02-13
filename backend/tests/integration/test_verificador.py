d7edfe9c4716fbc54bcf9ea3205d470d57f788e3
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-02-12 22:02:54 -0300

    tests: implementa testes de leitura, criacao e exclusao de verificador. (#207)

--- backend/tests/integration/test_verificador.py
+++ backend/tests/integration/test_verificador.py
@@ -1,6 +1,7 @@
 from tests.database import get_db_test
 from tests.helpers.administrador import create_administrador_helper
 from tests.helpers.user import create_user_helper
+from tests.helpers.verificador import create_verificador_admin_helper, create_verificador_user_helper
 from backend.main import app
 from fastapi.testclient import TestClient
 from tests.config_test import remove_dependencies, resume_dependencies
@@ -39,5 +40,99 @@ def test_read_verificadores_admin():
     )
 
     assert response_admin.status_code == 200
+
+    resume_dependencies()
+
+
+def test_read_verificador_unit_user():
+    remove_dependencies()
+
+    response_verificador, token = create_verificador_user_helper()
+    verificador_id = response_verificador.json().get("data").get("id")
+
+    response = client.get(
+        f"{URL_VERIFICADOR}/{verificador_id}/",
+        headers={
+            "Authorization": f"Bearer {token}",
+        },
+    )
+
+    assert response.status_code == 200
+
+    resume_dependencies()
+
+
+def test_read_verificador_unit_admin():
+    remove_dependencies()
+
+    response_verificador, token = create_verificador_admin_helper()
+    verificador_id = response_verificador.json().get("data").get("id")
+
+    response = client.get(
+        f"{URL_VERIFICADOR}/{verificador_id}/",
+        headers={
+            "Authorization": f"Bearer {token}",
+        },
+    )
+
+    assert response.status_code == 200
+
+    resume_dependencies()
+
+
+def test_create_verificador_user():
+    remove_dependencies()
+
+    response, _ = create_verificador_user_helper()
+
+    assert response.status_code == 201
+
+    resume_dependencies()
+
+
+def test_create_verificador_admin():
+    remove_dependencies()
+
+    response, _ = create_verificador_admin_helper()
+
+    assert response.status_code == 201
+
+    resume_dependencies()
+
+
+def test_delete_verificador_user():
+    remove_dependencies()
+
+    response_verificador, token = create_verificador_user_helper()
+
+    verificador_id = response_verificador.json().get("data").get("id")
+
+    response = client.delete(
+        f"{URL_VERIFICADOR}/{verificador_id}/",
+        headers={
+            "Authorization": f"Bearer {token}",
+        }
+    )
+
+    assert response.status_code == 200
+
+    resume_dependencies()
+
+
+def test_delete_verificador_admin():
+    remove_dependencies()
+
+    response_verificador, token = create_verificador_admin_helper()
+
+    verificador_id = response_verificador.json().get("data").get("id")
+
+    response = client.delete(
+        f"{URL_VERIFICADOR}/{verificador_id}/",
+        headers={
+            "Authorization": f"Bearer {token}",
+        }
+    )
+
+    assert response.status_code == 200
 
     resume_dependencies()
