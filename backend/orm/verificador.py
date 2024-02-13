e18dfe1b8065f2f34b738e54350bd006216bcf2b
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-02-12 21:47:47 -0300

    feat: implementa ORM de excluir verificador. (#207)

--- backend/orm/verificador.py
+++ backend/orm/verificador.py
@@ -51,3 +51,27 @@ async def create_verificador(
     except SQLAlchemyError:
         db.rollback()
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+async def delete_verificador(
+    db: Session,
+    id: int,
+    user: User | Administrador,
+):
+
+    db_verificador = db.query(Verificador).filter(Verificador.id == id).first()
+    if not db_verificador:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+    if (is_user(user) and user.id != db_verificador.problema.usuario_id):
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+
+    try:
+        db_verificador.problema.verificador_id = None
+        db.delete(db_verificador)
+        db.commit()
+        return db_verificador
+
+    except SQLAlchemyError:
+        db.rollback()
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR)
