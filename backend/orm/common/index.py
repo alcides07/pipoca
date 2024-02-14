f9e31b0b06b8d22a3e027632c45d3e50bc3e0e94
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-02-14 19:03:17 -0300

    feat: implementa metodo generico de filtragem. (#188)

--- backend/orm/common/index.py
+++ backend/orm/common/index.py
@@ -1,12 +1,55 @@
-from dependencies.authorization_user import has_authorization_object_single, has_authorization_object_collection, is_user
-from models.user import User
-from sqlalchemy import or_
+from enum import Enum
+from dependencies.authorization_user import has_authorization_object_single, has_authorization_object_collection, is_admin, is_user
+from schemas.common.direction_order_by import DirectionOrderByEnum
+from sqlalchemy import asc, desc, or_
 from sqlalchemy.exc import SQLAlchemyError
-from typing import Any
+from typing import Any, Optional
 from fastapi import HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, Query
 from schemas.common.pagination import MetadataSchema, PaginationSchema
 from fastapi import status
+
+
+def filter_collection(
+    model: Any,
+    filters: Any,
+    pagination: PaginationSchema,
+    query: Query[Any],
+    search_fields: Any,
+    field_order_by: Optional[Enum],
+    direction: Optional[DirectionOrderByEnum],
+):
+    total = query.count()
+
+    if (filters):
+        for attr, value in filters.__dict__.items():
+            if (hasattr(model, attr) and value != None):
+                query = query.filter(getattr(model, attr) == value)
+
+    if (pagination.q):
+        search_query = or_(
+            *[getattr(model, field).ilike(f"%{pagination.q}%") for field in search_fields if hasattr(model, field)])
+        query = query.filter(search_query)
+
+    if (field_order_by and hasattr(model, field_order_by.value)):
+        if (direction == DirectionOrderByEnum.DESC):
+            query = query.order_by(
+                desc(getattr(model, field_order_by.value)))
+        else:
+            query = query.order_by(
+                asc(getattr(model, field_order_by.value)))
+
+    query = query.offset(pagination.offset).limit(pagination.limit)
+
+    metadata = MetadataSchema(
+        count=query.count(),
+        total=total,
+        offset=pagination.offset,
+        limit=pagination.limit,
+        search_fields=search_fields
+    )
+
+    return query, metadata
 
 
 def get_by_key_value_exists(db: Session, model: Any, key: str, value):
@@ -47,41 +90,32 @@ async def get_all(
     model: Any,
     pagination: PaginationSchema,
     token: str,
+    field_order_by: Optional[Enum] = None,
+    direction: Optional[DirectionOrderByEnum] = None,
     filters: Any = None,
     search_fields: list[str] = [],
     allow_any: bool = False,
     me_author: bool = False
 ):
+    user = await user_autenthicated(token, db)
 
-    if (allow_any == False):
-        if (not await has_authorization_object_collection(db, token)):
-            raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+    if (allow_any == False and not is_admin(user)):
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
     query = db.query(model)
 
     if (me_author == True and hasattr(model, "usuario_id")):
-        user = await user_autenthicated(token, db)
         if (is_user(user)):
             query = db.query(model).filter(model.usuario_id == user.id)
 
-    if filters:
-        for attr, value in filters.__dict__.items():
-            if value != None:
-                query = query.filter(getattr(model, attr) == value)
-
-    if pagination.q and search_fields:
-        search_query = or_(
-            *[getattr(model, field).ilike(f"%{pagination.q}%") for field in search_fields if hasattr(model, field)])
-        query = query.filter(search_query)
-
-    db_objects = query.offset(pagination.offset).limit(pagination.limit)
-    total = query.count()
-    metadata = MetadataSchema(
-        count=db_objects.count(),
-        total=total,
-        offset=pagination.offset,
-        limit=pagination.limit,
-        search_fields=search_fields
+    db_objects, metadata = filter_collection(
+        model,
+        filters,
+        pagination,
+        query,
+        search_fields,
+        field_order_by,
+        direction
     )
 
     return db_objects.all(), metadata
