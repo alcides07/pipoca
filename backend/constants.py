d5be3c3dba101689618dddd9b768094538c10844
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-02-14 21:44:49 -0300

    feat: cria arquivo de constantes globais. (#188)

---
+++ backend/constants.py
@@ -0,0 +1,2 @@
+FIELDS_ORDER_BY_DESCRIPTION = "Lista de campos que podem ser ordenados"
+DIRECTION_ORDER_BY_DESCRIPTION = "Direção da ordenação. 'ASC' para ordem crescente e 'DESC' para ordem decrescente."
