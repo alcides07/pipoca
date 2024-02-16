f97018e2925f2d29f0f12d940e6ab2915ddb7762
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-02-15 21:40:17 -0300

    feat: implementa rotas de ValidadorTeste. (#212)

--- backend/routers/validadorTeste.py
+++ backend/routers/validadorTeste.py
@@ -1,46 +1,163 @@
+from models.validadorTeste import ValidadorTeste
+from orm.validadorTeste import create_validador_teste, delete_validador_teste, get_validador_teste_by_id, update_validador_teste
+from schemas.validadorTeste import VALIDADOR_TESTE_ID_DESCRIPTION, ValidadorTesteCreate, ValidadorTesteReadFull, ValidadorTesteReadSimple, ValidadorTesteUpdatePartial, ValidadorTesteUpdateTotal
+from routers.auth import oauth2_scheme
 from dependencies.authenticated_user import get_authenticated_user
-from fastapi import APIRouter, Depends, HTTPException, status
+from dependencies.database import get_db
+from fastapi import APIRouter, Body, Depends, Path, Response, status
+from orm.common.index import get_all
+from schemas.common.pagination import PaginationSchema
+from schemas.common.response import ResponsePaginationSchema, ResponseUnitSchema
+from sqlalchemy.orm import Session
+from utils.errors import errors
 
 
 router = APIRouter(
     prefix="/validadorTestes",
     tags=["validadorTestes"],
-    dependencies=[Depends(get_authenticated_user)],
-    deprecated=True
+    dependencies=[Depends(get_authenticated_user)]
 )
 
 
-@router.get("/")
+@router.get("/",
+            response_model=ResponsePaginationSchema[ValidadorTesteReadSimple],
+            summary="Lista testes de validadores"
+            )
 async def read(
+        db: Session = Depends(get_db),
+        pagination: PaginationSchema = Depends(),
+        token: str = Depends(oauth2_scheme)
 ):
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    validadores_testes, metadata = await get_all(
+        db=db,
+        model=ValidadorTeste,
+        pagination=pagination,
+        token=token
+    )
+
+    return ResponsePaginationSchema(
+        data=validadores_testes,
+        metadata=metadata
+    )
 
 
-@router.get("/{id}/")
+@router.get("/{id}/",
+            response_model=ResponseUnitSchema[ValidadorTesteReadFull],
+            summary="Lista um teste de um validador",
+            responses={
+                404: errors[404]
+            }
+            )
 async def read_id(
+        id: int = Path(description=VALIDADOR_TESTE_ID_DESCRIPTION),
+        db: Session = Depends(get_db),
+        token: str = Depends(oauth2_scheme)
 ):
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    validador_teste = await get_validador_teste_by_id(
+        db=db,
+        id=id,
+        token=token
+    )
+
+    return ResponseUnitSchema(
+        data=validador_teste
+    )
 
 
-@router.post("/")
+@router.post("/",
+             response_model=ResponseUnitSchema[ValidadorTesteReadFull],
+             status_code=201,
+             summary="Cadastra um teste para um validador",
+             responses={
+                 422: errors[422],
+                 404: errors[404]
+             },
+             )
 async def create(
+    validador_teste: ValidadorTesteCreate,
+    db: Session = Depends(get_db),
+    token: str = Depends(oauth2_scheme)
 ):
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    validador_teste = await create_validador_teste(
+        db=db,
+        validador_teste=validador_teste,
+        token=token
+    )
+
+    return ResponseUnitSchema(data=validador_teste)
 
 
-@router.patch("/{id}/")
+@router.patch("/{id}/",
+              response_model=ResponseUnitSchema[ValidadorTesteReadFull],
+              summary="Atualiza um teste de um validador parcialmente",
+              responses={
+                  400: errors[400],
+                  404: errors[404]
+              },
+              )
 async def parcial_update(
+        id: int = Path(description=VALIDADOR_TESTE_ID_DESCRIPTION),
+        db: Session = Depends(get_db),
+        data: ValidadorTesteUpdatePartial = Body(
+            description="Teste de validador a ser atualizado parcialmente"),
+        token: str = Depends(oauth2_scheme)
 ):
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    validador_teste = await update_validador_teste(
+        db=db,
+        id=id,
+        validador_teste=data,
+        token=token
+    )
+
+    return ResponseUnitSchema(
+        data=validador_teste
+    )
 
 
-@router.put("/{id}/")
+@router.put("/{id}/",
+            response_model=ResponseUnitSchema[ValidadorTesteReadFull],
+            summary="Atualiza um teste de um validador por completo",
+            responses={
+                400: errors[400],
+                404: errors[404]
+            },
+            )
 async def total_update(
+        id: int = Path(description=VALIDADOR_TESTE_ID_DESCRIPTION),
+        db: Session = Depends(get_db),
+        data: ValidadorTesteUpdateTotal = Body(
+            description="Teste de validador a ser atualizado por completo"),
+        token: str = Depends(oauth2_scheme)
 ):
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    validador_teste = await update_validador_teste(
+        db=db,
+        id=id,
+        validador_teste=data,
+        token=token
+    )
+
+    return ResponseUnitSchema(
+        data=validador_teste
+    )
 
 
-@router.delete("/{id}/")
+@router.delete("/{id}/",
+               status_code=204,
+               summary="Deleta um teste de um validador",
+               responses={
+                   404: errors[404]
+               }
+               )
 async def delete(
+        id: int = Path(description=VALIDADOR_TESTE_ID_DESCRIPTION),
+        db: Session = Depends(get_db),
+        token: str = Depends(oauth2_scheme)
 ):
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    validador_teste = await delete_validador_teste(
+        db=db,
+        id=id,
+        token=token
+    )
+
+    if (validador_teste):
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
