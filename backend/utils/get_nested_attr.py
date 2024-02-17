99342120bb72a594f8330535dace634c82a5d29b
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-02-17 03:51:28 -0300

    refactor: renomeia variavel para algo nao built-in. (#237)

--- backend/utils/get_nested_attr.py
+++ backend/utils/get_nested_attr.py
@@ -1,11 +1,11 @@
 from typing import Any
 
 
-def get_nested_attr(object: Any, path: str):
+def get_nested_attr(obj: Any, path: str):
     attrs = path.split('.')
     for attr in attrs:
-        if hasattr(object, attr):
-            object = getattr(object, attr)
+        if hasattr(obj, attr):
+            obj = getattr(object, attr)
         else:
             return None
-    return object
+    return obj
