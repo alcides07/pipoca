954392c2cc3c8bf8bdc6126ab935d33644338b73
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-02-17 04:08:17 -0300

    fix: corrige nome de variavel utilizada. (#237)

--- backend/utils/get_nested_attr.py
+++ backend/utils/get_nested_attr.py
@@ -5,7 +5,7 @@ def get_nested_attr(obj: Any, path: str):
     attrs = path.split('.')
     for attr in attrs:
         if hasattr(obj, attr):
-            obj = getattr(object, attr)
+            obj = getattr(obj, attr)
         else:
             return None
     return obj
