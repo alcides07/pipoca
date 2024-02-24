0d5fdbb23a3be8af00dc8ffab2d7f7eb27f6f45c
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-02-24 19:21:32 -0300

    feat: implementa metodo ORM update_simple. (#214)

--- backend/orm/common/index.py
+++ backend/orm/common/index.py
@@ -177,7 +177,7 @@ async def delete_object(
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-async def delete_simple(
+async def delete_object_simple(
     db: Session,
     model: Any,
     id: int
@@ -223,6 +223,31 @@ async def update_object(
             for key, value in data.model_dump().items():
                 if hasattr(db_object, key):
                     setattr(db_object, key, value)
+        db.commit()
+        db.refresh(db_object)
+
+        return db_object
+
+    except SQLAlchemyError:
+        db.rollback()
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+async def update_object_simple(
+    db: Session,
+    id: int,
+    model: Any,
+    data: Any
+):
+    db_object = db.query(model).filter(model.id == id).first()
+    if not db_object:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+    try:
+        for key, value in data.model_dump().items():
+            if (value != None and hasattr(db_object, key)):
+                setattr(db_object, key, value)
+
         db.commit()
         db.refresh(db_object)
 
