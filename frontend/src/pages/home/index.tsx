bc3bfeec267a3df4511372f5c2e0d7d82931503a
Israel Kleber <israel.lima@escolar.ifrn.edu.br>
2024-02-25 23:29:40 -0300

    feat: adiciona versão inicial do projeto com tailwind e shadcn/ui (#268)

---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default Home;
