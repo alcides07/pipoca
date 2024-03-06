6cc737badb0d9e9d30c7eb7617680ba8be08b4e4
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-03-05 21:14:05 -0300

    feat: define compilador com uso do checker. (#277)

--- backend/languages_run.py
+++ backend/languages_run.py
@@ -1,23 +1,28 @@
 FILENAME_RUN = "Main"
-INPUT_FILENAME = "input.txt"
+INPUT_TEST_FILENAME = "input.txt"
+OUTPUT_JUDGE_FILENAME = "output_judge.txt"
+OUTPUT_USER_FILENAME = "output_user.txt"
 
 commands = {
     ".py": {
         "image": "python:3",
+        "extension": ".py",
         "run": ["/bin/bash", "-c",
-                f"python {FILENAME_RUN}.py < {INPUT_FILENAME}"],
+                f"python {FILENAME_RUN}.py < {INPUT_TEST_FILENAME}"],
     },
 
     ".java": {
         "image": "openjdk:11",
+        "extension": ".java",
         "run": [
             "/bin/bash", "-c",
-            f"javac {FILENAME_RUN}.java && java {FILENAME_RUN} < {INPUT_FILENAME}"
+            f"javac {FILENAME_RUN}.java && java {FILENAME_RUN} < {INPUT_TEST_FILENAME}"
         ]
     },
 
     ".rb": {
         "image": "ruby:3",
+        "extension": ".rb",
         "run": [
             "/bin/bash", "-c",
             f"ruby {FILENAME_RUN}.rb"
@@ -26,9 +31,19 @@ commands = {
 
     ".cpp": {
         "image": "gcc",
+        "extension": ".cpp",
         "run": [
             "/bin/bash", "-c",
-            f"g++ -o {FILENAME_RUN} {FILENAME_RUN}.cpp && ./{FILENAME_RUN} < {INPUT_FILENAME}"
+            f"g++ -o {FILENAME_RUN} {FILENAME_RUN}.cpp && ./{FILENAME_RUN} < {INPUT_TEST_FILENAME}"
+        ]
+    },
+
+    "cpp.g++17": {
+        "image": "gcc",
+        "extension": ".cpp",
+        "run": [
+            "/bin/bash", "-c",
+            f"g++ -std=c++17 -o {FILENAME_RUN} {FILENAME_RUN}.cpp && ./{FILENAME_RUN} {INPUT_TEST_FILENAME} {OUTPUT_USER_FILENAME} {OUTPUT_JUDGE_FILENAME}"
         ]
     }
 }
