1e595c9c15cfae4a8aa3d2d56f6367bccceb1819
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-03-05 21:13:02 -0300

    docs: define constante de URL de test lib. (#277)

--- backend/constants.py
+++ backend/constants.py
@@ -1,2 +1,3 @@
 FIELDS_ORDER_BY_DESCRIPTION = "Lista de campos que podem ser ordenados"
 DIRECTION_ORDER_BY_DESCRIPTION = "Direção da ordenação. 'ASC' para ordem crescente e 'DESC' para ordem decrescente."
+URL_TEST_LIB = "https://raw.githubusercontent.com/MikeMirzayanov/testlib/master/testlib.h"
