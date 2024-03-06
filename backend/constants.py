ebe309cf095f724f0013a46f12147f1021f6061d
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-03-05 21:51:26 -0300

    feat: define filename em container como constantes. (#277)

--- backend/constants.py
+++ backend/constants.py
@@ -1,3 +1,9 @@
 FIELDS_ORDER_BY_DESCRIPTION = "Lista de campos que podem ser ordenados"
 DIRECTION_ORDER_BY_DESCRIPTION = "Direção da ordenação. 'ASC' para ordem crescente e 'DESC' para ordem decrescente."
+
 URL_TEST_LIB = "https://raw.githubusercontent.com/MikeMirzayanov/testlib/master/testlib.h"
+
+FILENAME_RUN = "Main"
+INPUT_TEST_FILENAME = "input.txt"
+OUTPUT_JUDGE_FILENAME = "output_judge.txt"
+OUTPUT_USER_FILENAME = "output_user.txt"
