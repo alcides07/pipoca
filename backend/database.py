944a20059c45953ce238b121a04a86913bde9432
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-03-07 20:46:47 -0300

    ops: padroniza postgres como bd local. (#277)

--- backend/database.py
+++ backend/database.py
@@ -2,15 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from decouple import config
 
-DATABASE_CONTAINER = str(config("DATABASE_CONTAINER"))
-DATABASE_LOCAL = str(config("DATABASE_LOCAL"))
-USE_DOCKER = config("USE_DOCKER")
+DATABASE_URL = str(config("DATABASE_URL"))
 
-engine = create_engine(DATABASE_LOCAL, connect_args={
-                       "check_same_thread": False})
-
-if (USE_DOCKER == "1"):
-    engine = create_engine(DATABASE_CONTAINER)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- backend/dependencies/database.py
+++ backend/dependencies/database.py
@@ -1,16 +1,9 @@
-from database import SessionLocal, engine
-from sqlalchemy import text
-from decouple import config
+from database import SessionLocal
 
 
 def get_db():
     db = SessionLocal()
     try:
-        USE_DOCKER = config("USE_DOCKER")
-
-        if (USE_DOCKER == "0"):
-            with engine.connect() as connection:
-                connection.execute(text('PRAGMA foreign_keys=ON'))
         yield db
     finally:
         db.close()
