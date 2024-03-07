9723c0b5f849210833582421b7256a35e1ae6979
Israel Kleber <israel.lima@escolar.ifrn.edu.br>
2024-03-07 11:42:01 -0300

    feat: adiciona login e register no sistema (#282)

--- frontend/src/pages/login/components/formLogin.tsx
+++ frontend/src/pages/login/components/formLogin.tsx
@@ -1,9 +1,9 @@
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -11,93 +11,106 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
 } from "@/components/ui/form";
 import AutenticacaoService from "@/service/api/autenticacaoService";
+import { useNavigate } from "react-router-dom";
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-  password: z.string().min(3, {
-    message: "Password must be at least 3 characters.",
-  }),
+	username: z.string().min(2, {
+		message: "Username must be at least 2 characters.",
+	}),
+	password: z.string().min(3, {
+		message: "Password must be at least 3 characters.",
+	}),
 });
 
 function FormLogin() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-      password: "",
-    },
-  });
+	const navigate = useNavigate();
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log("values", values);
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			username: "",
+			password: "",
+		},
+	});
 
-    const data = {
-      username: values.username,
-      password: values.password,
-    };
-    console.log("data", data);
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		console.log("values", values);
 
-    AutenticacaoService.login(data)
-      .then((response) => {
-        console.log("Login", response);
-      })
-      .catch((error) => {
-        console.error("Login erro:", error);
-      });
-  }
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Login</CardTitle>
-        <CardDescription>
-          Realize login para ter acesso ao sistema Pipoca!
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="space-y-2">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Nome</FormLabel>
-                  <FormControl>
-                    <Input placeholder="" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Senha</FormLabel>
-                  <FormControl>
-                    <Input type="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit">Entrar</Button>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
-  );
+		const data = {
+			username: values.username,
+			password: values.password,
+		};
+		console.log("data", data);
+
+		AutenticacaoService.login(data)
+			.then((response) => {
+				console.log("response", response);
+				if (response.status === 200) {
+					console.log("token", response.data.access_token);
+					localStorage.setItem(
+						"access_token",
+						response.data.access_token
+					);
+					navigate("/");
+				}
+			})
+			.catch((error) => {
+				console.error("Login erro:", error);
+			});
+	}
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Login</CardTitle>
+				<CardDescription>
+					Realize login para ter acesso ao sistema Pipoca!
+				</CardDescription>
+			</CardHeader>
+			<CardContent className="space-y-2">
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className="space-y-2">
+						<FormField
+							control={form.control}
+							name="username"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Nome</FormLabel>
+									<FormControl>
+										<Input placeholder="" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="password"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Senha</FormLabel>
+									<FormControl>
+										<Input type="password" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<Button type="submit">Entrar</Button>
+					</form>
+				</Form>
+			</CardContent>
+		</Card>
+	);
 }
 
 export default FormLogin;
--- frontend/src/service/axiosInstance.ts
+++ frontend/src/service/axiosInstance.ts
@@ -1,11 +1,25 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/",
-  headers: {
-    accept: "application/json",
-    Authorization: "Bearer ",
-  },
+	baseURL: "http://localhost:8000/",
 });
+
+function getToken(): string | null {
+	return localStorage.getItem("access_token");
+}
+
+axiosInstance.interceptors.request.use(
+	(config) => {
+		console.log("intercep:", config);
+		const accessToken = getToken();
+		if (accessToken != null) {
+			config.headers.Authorization = `Bearer ${accessToken}`;
+		}
+		return config;
+	},
+	async (error) => {
+		await Promise.reject(error);
+	}
+);
 
 export default axiosInstance;
