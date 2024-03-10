51c55a5061533604b8b304ef984bff334779f22e
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-03-10 00:47:47 -0300

    fix: corrige acesso a engine. (#277)

--- backend/database.py
+++ backend/database.py
@@ -6,6 +6,5 @@ TEST_ENV = str(config("TEST_ENV"))
 if (TEST_ENV != "1"):
     DATABASE_URL = str(config("DATABASE_URL"))
     engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
