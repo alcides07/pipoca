96e6896737a243d8dada21528786b7c072438874
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-03-10 00:44:22 -0300

    fix: corrige comparacao por TEST_ENV. (#277)

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from decouple import config
 
-TEST_ENV = config("TEST_ENV")
-if (TEST_ENV != 1):
+TEST_ENV = str(config("TEST_ENV"))
+if (TEST_ENV != "1"):
     DATABASE_URL = str(config("DATABASE_URL"))
     engine = create_engine(DATABASE_URL)
 
