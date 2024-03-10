cca18f28b434229f4d83bf4a1673df61710204f6
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-03-10 00:24:47 -0300

    feat: verifica ambiente de testes. (#277)

--- backend/database.py
+++ backend/database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from decouple import config
 
-DATABASE_URL = str(config("DATABASE_URL"))
-
-engine = create_engine(DATABASE_URL)
+TEST_ENV = config("TEST_ENV")
+if (TEST_ENV != 1):
+    DATABASE_URL = str(config("DATABASE_URL"))
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
