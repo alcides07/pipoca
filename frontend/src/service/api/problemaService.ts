91c4e6be1df800e2d1e51b8d619bb63c9200d8e3
Israel Kleber <israel.lima@escolar.ifrn.edu.br>
2024-03-21 12:04:37 -0300

    feat: adiciona importação de problema do polygon (#291)

--- frontend/src/service/api/problemaService.ts
+++ frontend/src/service/api/problemaService.ts
@@ -1,23 +1,24 @@
+import axios from "axios";
 import axiosInstance from "../axiosInstance";
 
 class ProblemaService {
-  async getProblemas() {
-    const response = await axiosInstance.get("/problemas/");
-    console.log(response.data);
-    return response.data;
-  }
-  async getProblemaById(id: string) {
-    const response = await axiosInstance.get(`/problemas/${id}`);
-    return response.data;
-  }
-  async uploadFile(formData: FormData) {
-    const response = await axiosInstance.post(`/problemas/upload/`, formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
-    return response.data;
-  }
+	async getProblemas() {
+		const response = await axiosInstance.get("/problemas/");
+		console.log(response.data);
+		return response.data;
+	}
+	async getProblemaById(id: string) {
+		const response = await axiosInstance.get(`/problemas/${id}`);
+		return response.data;
+	}
+	async uploadFile(formData: FormData) {
+		const response = await axios.post(`/problemas/upload/`, formData, {
+			headers: {
+				"Content-Type": "multipart/form-data",
+			},
+		});
+		return response.data;
+	}
 }
 
 export default new ProblemaService();
