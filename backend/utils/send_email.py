a9f588141f2d5f1473a6b32ce03575babf88f8ba
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-03-31 16:27:34 -0300

    feat: aborta envio de email em ambiente de testes. (#365)

--- backend/utils/send_email.py
+++ backend/utils/send_email.py
@@ -11,19 +11,23 @@ def send_email(
     corpo: str
 ):
     try:
-        mensagem = MIMEText(corpo, "html")
-        mensagem['From'] = remetente
-        mensagem['Subject'] = assunto
-        mensagem['To'] = destinatario
-        PASSWORD_EMAIL = str(config("PASSWORD_EMAIL"))
+        TEST_ENV = str(config("TEST_ENV"))
+        if (TEST_ENV != "1"):
+            mensagem = MIMEText(corpo, "html")
+            mensagem['From'] = remetente
+            mensagem['Subject'] = assunto
+            mensagem['To'] = destinatario
+            PASSWORD_EMAIL = str(config("PASSWORD_EMAIL"))
 
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
-            smtp_server.login(remetente, PASSWORD_EMAIL)
-            smtp_server.sendmail(
-                from_addr=remetente,
-                to_addrs=destinatario,
-                msg=mensagem.as_string()
-            )
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+                smtp_server.login(remetente, PASSWORD_EMAIL)
+                smtp_server.sendmail(
+                    from_addr=remetente,
+                    to_addrs=destinatario,
+                    msg=mensagem.as_string()
+                )
+
+        return
 
     except Exception:
         raise HTTPException(
