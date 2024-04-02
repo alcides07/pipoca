2746e9d90a8548250bf989cdd00148c026d80074
Israel Kleber <israel.lima@escolar.ifrn.edu.br>
2024-04-02 14:44:37 -0300

    feat: adicionar versão final do formulário de cadastro (#331)

--- frontend/src/pages/Problemas/CadastroProblema/index.tsx
+++ frontend/src/pages/Problemas/CadastroProblema/index.tsx
@@ -1,54 +1,13 @@
-// import { Card, CardContent } from "@/components/ui/card";
-// import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-// import FormLogin from "../login/components/formLogin";
-// import FormRegister from "../login/components/formRegister";
-// import { Toaster } from "@/components/ui/toaster";
 import FormCadastro from "./components/formCadastro";
 
 function CadastroProblema() {
-  return (
-    <div className="mx-28 my-5">
-      {/* <div className="py-4">
-        <p className="text-2xl font-bold">Cadastro de problema</p>
-      </div>
-      <Tabs className="w-full">
-        <TabsList className="grid w-full grid-cols-6">
-          <TabsTrigger value="problema">Problema</TabsTrigger>
-          <TabsTrigger value="declaracao">Declaração</TabsTrigger>
-          <TabsTrigger value="arquivos">Arquivos</TabsTrigger>
-          <TabsTrigger value="validador">Validador</TabsTrigger>
-          <TabsTrigger value="validador">Verificador</TabsTrigger>
-          <TabsTrigger value="testes">Testes</TabsTrigger>
-        </TabsList>
-        <Card className="h-[80vh] flex justify-center items-center my-5">
-          <CardContent className="flex flex-col justify-center items-center">
-            <div className="w-[50vw] flex justify-center">
-              <TabsContent value="problema">
-                <FormLogin />
-              </TabsContent>
-              <TabsContent value="declaracao">
-                <FormLogin />
-              </TabsContent>
-              <TabsContent value="arquivos">
-                <FormRegister />
-              </TabsContent>
-              <TabsContent value="validador">
-                <FormRegister />
-              </TabsContent>
-              <TabsContent value="validador">
-                <FormLogin />
-              </TabsContent>
-              <TabsContent value="testes">
-                <FormRegister />
-              </TabsContent>
-            </div>
-          </CardContent>
-        </Card>
-      </Tabs>
-      <Toaster /> */}
-      <FormCadastro />
-    </div>
-  );
+	return (
+		<div className="flex h-[90vh] items-center justify-center">
+			<div className="w-full max-w-5xl mx-auto p-4">
+				<FormCadastro />
+			</div>
+		</div>
+	);
 }
 
 export default CadastroProblema;
