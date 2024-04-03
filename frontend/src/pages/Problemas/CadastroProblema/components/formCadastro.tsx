3c563f7fdf125c319231db288fea7ddf6300f974
Israel Kleber <94148869+IsraelKleber@users.noreply.github.com>
2024-04-03 02:08:25 -0300

    fix: ajusta mensagem de sucesso (#331)

--- frontend/src/pages/Problemas/CadastroProblema/components/formCadastro.tsx
+++ frontend/src/pages/Problemas/CadastroProblema/components/formCadastro.tsx
@@ -101,12 +101,8 @@ function FormCadastro() {
       navigate(`/problema/${response.data.data.id}`);
 
       toast({
-        title: "You submitted the following values:",
-        description: (
-          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-            <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-          </pre>
-        ),
+        title: "Sucesso",
+        description: "Problema criado!",
       });
     });
   }
