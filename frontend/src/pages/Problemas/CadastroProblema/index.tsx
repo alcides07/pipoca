e2c0dd19e16f3881ff180b7e002fb0fd54df41fe
Israel Kleber <94148869+IsraelKleber@users.noreply.github.com>
2024-04-02 23:31:15 -0300

    fix: ajusta posicionamento do componente de formulário (#331)

--- frontend/src/pages/Problemas/CadastroProblema/index.tsx
+++ frontend/src/pages/Problemas/CadastroProblema/index.tsx
@@ -1,13 +1,13 @@
 import FormCadastro from "./components/formCadastro";
 
 function CadastroProblema() {
-	return (
-		<div className="flex h-[90vh] items-center justify-center">
-			<div className="w-full max-w-5xl mx-auto p-4">
-				<FormCadastro />
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex h-[90vh] items-center justify-center mt-16">
+      <div className="w-full max-w-5xl mx-auto p-4">
+        <FormCadastro />
+      </div>
+    </div>
+  );
 }
 
 export default CadastroProblema;
