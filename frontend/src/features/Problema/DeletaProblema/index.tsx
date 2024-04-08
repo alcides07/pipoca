9e37e4c9297c8f54fbc6a67e20890e13072dfe51
Israel Kleber <94148869+IsraelKleber@users.noreply.github.com>
2024-04-08 02:18:02 -0300

    feat: adiciona pasta da funcionalidade de deletar problema (#361)

---
+++ frontend/src/features/Problema/DeletaProblema/index.tsx
@@ -0,0 +1,11 @@
+function DeletaProblema() {
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <h1 className="text-2xl font-bold">DeletaProblema</h1>
+      </div>
+    </>
+  );
+}
+
+export default DeletaProblema;
