0a015686395f6f1d8cce4c7b0ff4f55085030292
Israel Kleber <94148869+IsraelKleber@users.noreply.github.com>
2024-04-08 22:48:32 -0300

    fix: ajusta rota de edição (#352)

--- frontend/src/components/table/columns/problemaColumns.tsx
+++ frontend/src/components/table/columns/problemaColumns.tsx
@@ -1,7 +1,6 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { Edit2, ClipboardPenLine } from "lucide-react";
 import { iDataProblema } from "../../../interfaces/models/iProblema";
-import { ArrowUpDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
@@ -23,13 +22,13 @@ export const problemaColumns: ColumnDef<iDataProblema>[] = [
     cell: ({ row }) => {
       const problema = row.original;
       return (
-        <div className="w-5 flex flex-row gap-3">
+        <div className="px-5 w-5 flex flex-row gap-3">
           <Link to={`/problema/${problema.id}/responde/`}>
             <Button variant="outline" title="Responder">
               <ClipboardPenLine />
             </Button>
           </Link>
-          <Link to={`/editar/${problema.id}`}>
+          <Link to={`/problema/${problema.id}`}>
             <Button variant="outline" title="Editar">
               <Edit2 />
             </Button>
