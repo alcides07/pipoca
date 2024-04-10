c74d6008027320a469799336b1c7f0ef503b501c
Israel Kleber <israel.lima@escolar.ifrn.edu.br>
2024-04-10 10:34:18 -0300

    fix: ajusta interface de declaração (#372)

--- frontend/src/interfaces/iDeclaracao.ts
+++ frontend/src/interfaces/iDeclaracao.ts
@@ -1,18 +1,17 @@
 export interface iDeclaracoesProblema {
-  titulo: string;
-  idioma: string;
-  id: number;
-  problema_id: number;
+	titulo: string;
+	idioma: string;
+	id: number;
+	problema_id: number;
 }
 
 export interface iDeclaracao {
-  titulo: string;
-  idioma: string;
-  contextualizacao: string;
-  formatacao_entrada: string;
-  formatacao_saida: string;
-  observacao: string;
-  tutorial: string;
-  id?: number;
-  problema_id: number;
+	titulo: string;
+	idioma: string;
+	contextualizacao: string;
+	formatacao_entrada: string;
+	formatacao_saida: string;
+	observacao: string;
+	tutorial: string;
+	id?: number;
 }
