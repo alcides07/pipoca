a748972119ef34ab47f273a800bf3a1414b2a870
Israel Kleber <94148869+IsraelKleber@users.noreply.github.com>
2024-04-11 23:34:50 -0300

    chore: cria classe de serviço de declaração (#372)

--- frontend/src/services/models/declaracaoService.ts
+++ frontend/src/services/models/declaracaoService.ts
@@ -1,20 +1,26 @@
-import { iDeclaracao } from "../../interfaces/iDeclaracao";
+import { iDeclaracao } from "@/interfaces/models/iDeclaracao";
 import axiosInstance from "../axiosInstance";
 
 class DeclaracaoService {
-	async getDeclaracao(id: number) {
-		const response = await axiosInstance.get(`/declaracoes/${id}`);
-		return response.data;
-	}
-	async getDeclaracoes() {
-		const response = await axiosInstance.get("/declaracoes/");
-		return response.data;
-	}
-	async createDeclaracao(data: iDeclaracao) {
-		const response = await axiosInstance.post(`/declaracoes/`, data);
-		console.log(response);
-		return response;
-	}
+  async getDeclaracao(id: number) {
+    const response = await axiosInstance.get(`/declaracoes/${id}`);
+    return response.data;
+  }
+
+  async atualizaDeclaracao(id: number, data: iDeclaracao) {
+    const response = await axiosInstance.put(`/declaracoes/${id}`, data);
+    return response.data;
+  }
+
+  async getDeclaracoes() {
+    const response = await axiosInstance.get("/declaracoes/");
+    return response.data;
+  }
+  async createDeclaracao(data: iDeclaracao) {
+    const response = await axiosInstance.post(`/declaracoes/`, data);
+    console.log(response);
+    return response;
+  }
 }
 
 export default new DeclaracaoService();
