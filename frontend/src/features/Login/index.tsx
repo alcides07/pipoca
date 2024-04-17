6dc66cb6f0d082c0d89610557c0b522dcd8f0f8a
Israel Kleber <israel.lima@escolar.ifrn.edu.br>
2024-04-17 15:03:14 -0300

    fix: ajusta posição do formulário de cadastro (#429)

--- frontend/src/features/Login/index.tsx
+++ frontend/src/features/Login/index.tsx
@@ -5,36 +5,37 @@ import FormRegister from "./components/formRegister";
 import { Toaster } from "@/components/ui/toaster";
 
 function Login({ onLogin }: any) {
-  const [activeTab, setActiveTab] = useState("login");
+	const [activeTab, setActiveTab] = useState("login");
 
-  const handleRegisterSuccess = () => {
-    setActiveTab("login");
-  };
+	const handleRegisterSuccess = () => {
+		setActiveTab("login");
+	};
 
-  return (
-    <>
-      <Tabs value={activeTab} className="w-[400px] min-h-auto ">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="login" onClick={() => setActiveTab("login")}>
-            Login
-          </TabsTrigger>
-          <TabsTrigger
-            value="cadastro"
-            onClick={() => setActiveTab("cadastro")}
-          >
-            Cadastro
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="login">
-          <FormLogin onLogin={onLogin} />
-        </TabsContent>
-        <TabsContent value="cadastro">
-          <FormRegister onSuccess={handleRegisterSuccess} />
-        </TabsContent>
-      </Tabs>
-      <Toaster />
-    </>
-  );
+	return (
+		<div className="h-auto min-h-[93vh] w-full flex  justify-center  items-center">
+			<Tabs value={activeTab} className="w-[400px] min-h-auto">
+				<TabsList className="grid w-full grid-cols-2">
+					<TabsTrigger
+						value="login"
+						onClick={() => setActiveTab("login")}>
+						Login
+					</TabsTrigger>
+					<TabsTrigger
+						value="cadastro"
+						onClick={() => setActiveTab("cadastro")}>
+						Cadastro
+					</TabsTrigger>
+				</TabsList>
+				<TabsContent value="login">
+					<FormLogin onLogin={onLogin} />
+				</TabsContent>
+				<TabsContent value="cadastro">
+					<FormRegister onSuccess={handleRegisterSuccess} />
+				</TabsContent>
+			</Tabs>
+			<Toaster />
+		</div>
+	);
 }
 
 export default Login;
