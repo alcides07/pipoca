9ebb5dc5fd166ea8bbf4bc640cb677068207ab7d
Israel Kleber <94148869+IsraelKleber@users.noreply.github.com>
2024-05-09 20:55:47 -0300

    feat: adiciona classe de serviço de atualização de um verificador (#375)

--- frontend/src/services/models/verificadorService.ts
+++ frontend/src/services/models/verificadorService.ts
@@ -6,6 +6,10 @@ class VerificadorService {
     const response = await axiosInstance.post(`/verificadores/`, data);
     return response;
   }
+  async atualizaVerificador(id: number, data: iVerificador) {
+    const response = await axiosInstance.put(`/verificadores/${id}`, data);
+    return response.data;
+  }
   async consultaTesteVerificador(id: number) {
     const response = await axiosInstance.get(`/verificadores/${id}/testes`);
     return response.data;
