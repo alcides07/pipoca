03e05d0aec75f3b87ab220b31dce9ffbb7765ccd
Israel Kleber <94148869+IsraelKleber@users.noreply.github.com>
2024-05-19 21:01:23 -0300

    fix: ajusta baseURL

--- frontend/src/services/axiosInstance.ts
+++ frontend/src/services/axiosInstance.ts
@@ -1,25 +1,24 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "http://localhost:8000/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 axiosInstance.interceptors.request.use(
-	(config) => {
-		const token = localStorage.getItem("access_token");
-		if (token) {
-			config.headers["Authorization"] = `Bearer ${token}`;
-		} else {
-			if (config.url?.includes("/login")) {
-				config.headers["Content-Type"] =
-					"application/x-www-form-urlencoded";
-			}
-		}
-		return config;
-	},
-	(error) => {
-		return Promise.reject(error);
-	}
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    } else {
+      if (config.url?.includes("/login")) {
+        config.headers["Content-Type"] = "application/x-www-form-urlencoded";
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default axiosInstance;
