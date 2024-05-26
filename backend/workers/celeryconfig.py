1e9fe11275996df3ddf17cb576282f8fdba2c25f
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-05-26 00:27:46 -0300

    feat: configura celery. (#456)

---
+++ backend/workers/celeryconfig.py
@@ -0,0 +1,11 @@
+from decouple import config
+
+DATABASE_URL = str(config("DATABASE_URL"))
+BROKER_URL = str(config("BROKER_URL"))
+
+broker_url = BROKER_URL
+result_backend = f'db+{DATABASE_URL}'
+task_serializer = 'json'
+result_serializer = 'json'
+accept_content = ['json']
+enable_utc = True
