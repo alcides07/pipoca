84044d879a3301d4f410cfc60a78c753723a45fb
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-05-26 21:42:27 -0300

    feat: redireciona confirmacao de email para frontend. (#466)

--- backend/orm/user.py
+++ backend/orm/user.py
@@ -279,14 +279,14 @@ async def create_token_ativacao_conta_and_send_email(
     destinatario: str,
 ):
     EXPIRE_MINUTES = 15
-    API_BASE_URL = str(config("API_BASE_URL"))
+    FRONT_BASE_URL = str(config("FRONT_BASE_URL"))
     access_token_expires = timedelta(minutes=EXPIRE_MINUTES)
     token = create_token(
         data=data_token,
         expires_delta=access_token_expires
     )
 
-    url_ativacao = f"{API_BASE_URL}/auth/ativacao/?codigo={token}"
+    url_ativacao = f"{FRONT_BASE_URL}?codigo={token}"
 
     send_email(
         remetente="plataformapipoca@gmail.com",
