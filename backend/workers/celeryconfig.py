8c02b091c4c12c8fe2749ab3c18560e824941e0c
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-06-08 23:12:25 -0300

    refactor: renomeia filas explicitamente. (#456)

--- backend/workers/celeryconfig.py
+++ backend/workers/celeryconfig.py
@@ -1,4 +1,5 @@
 from decouple import config
+from kombu import Queue
 
 DATABASE_URL = str(config("DATABASE_URL"))
 BROKER_URL = str(config("BROKER_URL"))
@@ -10,3 +11,6 @@ result_serializer = 'json'
 accept_content = ['json']
 enable_utc = True
 task_track_started = True
+
+correcao_problema_queue = Queue("correcao-problema")
+task_queues = (correcao_problema_queue,)
