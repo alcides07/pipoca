8985791684763b8ca6eadf37f7cc4c81654cab60
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-06-09 22:37:07 -0300

    ops: isola env de password email. (#456)

--- backend/enviroments.py
+++ backend/enviroments.py
@@ -4,10 +4,12 @@ ENV = str(config("ENV"))
 ALGORITHM = str(config("ALGORITHM"))
 SECRET_KEY = str(config("SECRET_KEY"))
 VERSION_API = str(config("VERSION_API", default="0.0.0"))
-PASSWORD_EMAIL = str(config("PASSWORD_EMAIL"))
 TOKEN_EXPIRE_MINUTES = float(config("TOKEN_EXPIRE_MINUTES"))
 
 DATABASE_URL = str(config("DATABASE_URL"))
 FRONT_BASE_URL = str(config("FRONT_BASE_URL"))
 API_BASE_URL = str(config("API_BASE_URL"))
 BROKER_URL = str(config("BROKER_URL"))
+
+if (ENV == "production"):
+    PASSWORD_EMAIL = str(config("PASSWORD_EMAIL"))
