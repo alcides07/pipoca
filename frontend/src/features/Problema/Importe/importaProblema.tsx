4a8d729b88a1405e06fb6e378f6045fba05aa444
Israel Kleber <94148869+IsraelKleber@users.noreply.github.com>
2024-06-11 23:03:29 -0300

    wip: adiciona input de linguagens no import (#484)

--- frontend/src/features/Problema/Importe/importaProblema.tsx
+++ frontend/src/features/Problema/Importe/importaProblema.tsx
@@ -2,6 +2,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -25,10 +26,18 @@ import problemaService from "@/services/models/problemaService";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { toast } from "react-toastify";
+import linguagensPadrao from "@/utils/linguagem";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
 
 const FormSchema = z.object({
   privado: z.boolean().default(false).optional(),
   pacote: z.instanceof(FileList),
+  linguagens: z
+    .array(z.string())
+    .refine((value: string[]) => value.some((item) => item), {
+      message: "Selecione no mínimo uma linguagem.",
+    }),
 });
 
 interface importaProblemaProps {
@@ -48,9 +57,18 @@ function ImportaProblema({ handleProblem }: importaProblemaProps): JSX.Element {
   const fileRef = form.register("pacote");
 
   function onSubmit(values: z.infer<typeof FormSchema>) {
+    console.log("values 1", values);
+
     const formData = new FormData();
     formData.append("pacote", values.pacote[0]);
     formData.append("privado", String(values.privado));
+    values.linguagens.forEach((linguagem: string, index: number) => {
+      formData.append(`linguagens[${index}]`, linguagem);
+    });
+
+    for (var pair of formData.entries()) {
+      console.log(pair[0] + ", " + pair[1]);
+    }
 
     problemaService
       .uploadFile(formData)
@@ -97,23 +115,81 @@ function ImportaProblema({ handleProblem }: importaProblemaProps): JSX.Element {
                 );
               }}
             />
-            <FormField
-              control={form.control}
-              name="privado"
-              render={({ field }) => (
-                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md p-3 m-0">
-                  <FormControl>
-                    <Checkbox
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
-                    />
-                  </FormControl>
-                  <div className="leading-none">
-                    <FormLabel>Privado?</FormLabel>
-                  </div>
-                </FormItem>
-              )}
-            />
+            <div className="grid grid-cols-2 gap-4 pt-5">
+              <FormField
+                control={form.control}
+                name="linguagens"
+                render={() => (
+                  <FormItem>
+                    <div className="mb-4">
+                      <FormLabel className="text-base">Linguagens</FormLabel>
+                      <FormDescription>
+                        Selecione as linguagens aceitas na resposta do problema.
+                      </FormDescription>
+                    </div>
+                    {linguagensPadrao.map((linguagem) => (
+                      <FormField
+                        key={linguagem}
+                        control={form.control}
+                        name="linguagens"
+                        render={({ field }) => {
+                          return (
+                            <FormItem
+                              key={linguagem}
+                              className="flex flex-row items-start space-x-3 space-y-0"
+                            >
+                              <FormControl>
+                                <Checkbox
+                                  checked={
+                                    field.value
+                                      ? field.value.includes(linguagem)
+                                      : false
+                                  }
+                                  onCheckedChange={(checked) => {
+                                    return checked
+                                      ? field.onChange([
+                                          ...(field.value || []),
+                                          linguagem,
+                                        ])
+                                      : field.onChange(
+                                          (field.value || []).filter(
+                                            (value) => value !== linguagem
+                                          )
+                                        );
+                                  }}
+                                />
+                              </FormControl>
+                              <FormLabel className="text-sm font-normal">
+                                {linguagem}
+                              </FormLabel>
+                            </FormItem>
+                          );
+                        }}
+                      />
+                    ))}
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="privado"
+                render={({ field }) => (
+                  <FormItem className="col-span-1">
+                    <FormControl>
+                      <div className="flex items-center space-x-2">
+                        <Switch
+                          // checked={field.value}
+                          onCheckedChange={field.onChange}
+                        />
+                        <Label htmlFor="airplane-mode">Privado</Label>
+                      </div>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
           </form>
         </Form>
         <AlertDialogFooter>
