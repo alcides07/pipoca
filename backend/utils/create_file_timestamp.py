da7e15c0e3eb52f71de4379f982b04f3bf750598
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2024-09-01 20:41:52 -0300

    fix: verifica existencia de temp dir. (#555)

---
+++ backend/utils/create_file_timestamp.py
@@ -0,0 +1,18 @@
+import os
+from datetime import datetime
+from fastapi import UploadFile
+
+
+def create_file_timestamp(upload_file: UploadFile):
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    directory = "/API/temp"
+    file_path = os.path.join(
+        directory, f"arquivo_{timestamp}_{upload_file.filename}")
+
+    os.makedirs(directory, exist_ok=True)
+
+    with open(file_path, 'wb') as file:
+        content = upload_file.file.read()
+        file.write(content)
+
+    return file_path
